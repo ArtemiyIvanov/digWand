9cac182700b7b8db43b45ca40ee269fc02a98a52
a.abramovskiy <an.abramovskiy@gmail.ru>
2021-07-14 11:53:22 +0300

    lesson2

---
+++ examples/config/database.php
@@ -0,0 +1,17 @@
+<?php
+
+const DB_HOST = 'localhost';
+const DB_PORT = '3307';
+const DB_USER = 'root';
+const DB_PW = '';
+const DB_NAME = 'examples';
+const DB_CHAR = 'utf8';
+
+$config = [
+    'DB_HOST' => 'localhost',
+    'DB_PORT' => '3307',
+    'DB_USER' => 'root',
+    'DB_PW'   => '',
+    'DB_NAME' => 'examples',
+    'DB_CHAR' => 'utf8',
+];
--- examples/lesson1/index.php
+++ examples/lesson1/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('./config/database.php');
+require_once('../config/database.php');
 
 // $connect = mysqli_connect(DB_HOST.':'.DB_PORT, DB_USER, DB_PW, DB_NAME);
 
